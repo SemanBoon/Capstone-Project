32a838265824cf1a324d643369b25574286f679a
SemanBoon <134331970+SemanBoon@users.noreply.github.com>
2024-07-22 15:25:27 -0700

    added service page functionlity

--- frontend/src/Components/ProviderNavBar/ProviderNavBar.jsx
+++ frontend/src/Components/ProviderNavBar/ProviderNavBar.jsx
@@ -1,4 +1,3 @@
-
 import React, {useContext} from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from "../../UserContext";
@@ -32,5 +31,3 @@ const ProviderNavBar = ({id}) => {
 
 
 export default ProviderNavBar;
-
-
